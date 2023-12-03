b2723e8dfd3042a17b3354f142a1cb8bb5f2f400
zhujason <zhujason4@gmail.com>
2023-12-02 21:20:46 -0800

    changed setup

--- cpsc304project/src/sql/scripts/databaseSetup.sql
+++ cpsc304project/src/sql/scripts/databaseSetup.sql
@@ -6,6 +6,7 @@ CREATE TABLE Guest (
                        PRIMARY KEY (guest_name, ticket_number)
 );
 
+
 CREATE TABLE Staff (
                        staff_id INTEGER,
                        staff_role VARCHAR2(255), -- Assuming VARCHAR2 for role
@@ -14,6 +15,7 @@ CREATE TABLE Staff (
                        name VARCHAR2(255), -- Assuming VARCHAR2 for name
                        PRIMARY KEY (staff_id, name)
 );
+
 
 CREATE TABLE Sponsor (
                          sponsor_name VARCHAR2(255),
@@ -24,6 +26,7 @@ CREATE TABLE Sponsor (
                          PRIMARY KEY (sponsor_name, email)
 );
 
+
 CREATE TABLE Performer (
                            performer_name VARCHAR2(255) PRIMARY KEY, -- Assuming VARCHAR2 for performer_name
                            agent VARCHAR2(255),
@@ -31,12 +34,14 @@ CREATE TABLE Performer (
                            email VARCHAR2(255)
 );
 
+
 CREATE TABLE Event (
-                       event_time VARCHAR(255), -- Assuming TIMESTAMP for event_time
+                       event_time VARCHAR2(255),
                        event_id INTEGER PRIMARY KEY,
                        event_name VARCHAR2(255),
                        event_description VARCHAR2(255)
 );
+
 
 CREATE TABLE Department (
                             dept_name VARCHAR2(255),
@@ -46,15 +51,17 @@ CREATE TABLE Department (
                             FOREIGN KEY (event_id) REFERENCES Event(event_id)
 );
 
+
 CREATE TABLE Venue (
                        venue_name VARCHAR2(255),
                        venue_address VARCHAR2(255),
                        venue_capacity INTEGER,
                        event_id INTEGER,
-                       PRIMARY KEY (venue_name, venue_address, event_id),
+                       PRIMARY KEY (venue_name),
                        FOREIGN KEY (event_id) REFERENCES EVENT
-                       ON DELETE CASCADE
+                           ON DELETE CASCADE
 );
+
 
 CREATE TABLE Vendor (
                         vendor_name VARCHAR2(255),
@@ -62,6 +69,7 @@ CREATE TABLE Vendor (
                         vendor_hours INTEGER,
                         PRIMARY KEY (vendor_name)
 );
+
 
 CREATE TABLE Food_Vendor (
                              vendor_name VARCHAR2(255),
@@ -71,12 +79,14 @@ CREATE TABLE Food_Vendor (
                              FOREIGN KEY (vendor_name) REFERENCES Vendor(vendor_name)
 );
 
+
 CREATE TABLE Merchandise_Vendor (
                                     vendor_name VARCHAR2(255),
                                     brands_carried VARCHAR2(255),
                                     PRIMARY KEY (vendor_name),
                                     FOREIGN KEY (vendor_name) REFERENCES Vendor(vendor_name)
 );
+
 
 -- Your INSERT statements remain the same
 INSERT INTO Guest (guest_name, email, ticket_number, phone_number)
@@ -90,6 +100,7 @@ VALUES('Jason Zhu', 'jason.zhu@gmail.com', 1004, 123456789);
 INSERT INTO Guest (guest_name, email, ticket_number, phone_number)
 VALUES('Stephen Qiao', 'stephen.qiao@gmail.com', 1005, 123456789);
 
+
 INSERT INTO Staff (staff_id, name, email, phone_number, staff_role)
 VALUES (1, 'Leo Wang', 'leo.wang@gmail.com', 123456789, 'Manager');
 INSERT INTO Staff (staff_id, name, email, phone_number, staff_role)
@@ -100,6 +111,7 @@ INSERT INTO Staff (staff_id, name, email, phone_number, staff_role)
 VALUES(4, 'Jason Zhu', 'jason.zhu@gmail.com', 123456789, 'Coordinator');
 INSERT INTO Staff (staff_id, name, email, phone_number, staff_role)
 VALUES(5, 'Stephen Qiao', 'stephen.qiao@gmail.com', 123456789, 'Support Staff');
+
 
 INSERT INTO Sponsor (sponsor_name, amount, request, phone_number, email)
 VALUES('Facebook', 10000, 'Display advertisement at Main Stage', 123456789, 'facebook@gmail.com');
@@ -112,6 +124,7 @@ VALUES('Apple', 12000, 'Video sponsor', 123456789, 'apple@gmail.com');
 INSERT INTO Sponsor (sponsor_name, amount, request, phone_number, email)
 VALUES('Alibaba', 8000, 'Advertisement', 123456789, 'alibaba@gmail.com');
 
+
 INSERT INTO Performer (performer_name, agent, phone_number, email)
 VALUES('NBA Youngboy', 'Rich Paul', 123456789, 'youngboyneverbrokeagain@gmail.com');
 INSERT INTO Performer (performer_name, agent, phone_number, email)
@@ -122,6 +135,7 @@ INSERT INTO Performer (performer_name, agent, phone_number, email)
 VALUES('Kanye West', 'Pete Davidson', 123456789, 'ye@gmail.com');
 INSERT INTO Performer (performer_name, agent, phone_number, email)
 VALUES('Taylor Swift', 'Katy Perry', 123456789, 'taylorswift@gmail.com');
+
 
 INSERT INTO Event (event_time, event_id, event_name, event_description)
 VALUES('2023-11-01 08:00:00', 1, 'NBA Youngboy Concert', 'Concert for rapper NBA Youngboy ');
@@ -134,6 +148,7 @@ VALUES('2023-11-04 14:00:00', 4, 'Drake Concert', 'live performance by the 6ix g
 INSERT INTO Event (event_time, event_id, event_name, event_description)
 VALUES('2023-11-05 12:00:00', 5, 'WNBA game', 'New York Liberty vs. Las Vegas Aces');
 
+
 INSERT INTO Department (dept_name, dept_description, event_id)
 VALUES('Tech', 'Controls event technology', 1);
 INSERT INTO Department (dept_name, dept_description, event_id)
@@ -145,6 +160,7 @@ VALUES('Audio', 'Controls event audio', 4);
 INSERT INTO Department (dept_name, dept_description, event_id)
 VALUES('Bookings', 'Manages bookings', 5);
 
+
 INSERT INTO Venue (venue_name, venue_address, venue_capacity, event_id)
 VALUES('Apple', '123 Granville Street', 500, 1);
 INSERT INTO Venue (venue_name, venue_address, venue_capacity, event_id)
@@ -155,6 +171,7 @@ INSERT INTO Venue (venue_name, venue_address, venue_capacity, event_id)
 VALUES('Auto Group', '101 Student Union Blvd', 1000, 4);
 INSERT INTO Venue (venue_name, venue_address, venue_capacity, event_id)
 VALUES('Merch Store', '202 Lougheed Circle', 1500, 5);
+
 
 INSERT INTO Vendor (vendor_name, vendor_location, vendor_hours)
 VALUES('VenderA', 'North Side', 8);
@@ -177,6 +194,7 @@ VALUES('VenderI', 'Central Area', 10);
 INSERT INTO Vendor (vendor_name, vendor_location, vendor_hours)
 VALUES('VenderJ', 'Central Area', 10);
 
+
 INSERT INTO Food_Vendor (vendor_name, alcohol_number, style)
 VALUES('VenderA', 1001, 'Italian');
 INSERT INTO Food_Vendor (vendor_name, alcohol_number, style)
@@ -187,6 +205,7 @@ INSERT INTO Food_Vendor (vendor_name, alcohol_number, style)
 VALUES('VenderD', 1004, 'Indian');
 INSERT INTO Food_Vendor (vendor_name, alcohol_number, style)
 VALUES('VenderE', 1005, 'French');
+
 
 INSERT INTO Merchandise_Vendor (vendor_name, brands_carried)
 VALUES('VenderF', 'Nike');
